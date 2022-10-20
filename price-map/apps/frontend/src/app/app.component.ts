7dfbb9f07fa2466820eb9f88a58ba1377d7e2f71
Diana Kozachenko <kozachenkodiana03@mail.ru>
2022-10-20 20:28:47 +0700

    Regenerated apps

--- price-map/apps/frontend/src/app/app.component.ts
+++ price-map/apps/frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pm-front-root',
+  selector: 'price-map-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
