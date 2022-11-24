0920bf63a9d8894bb04a529f2134993ffb3d5624
Diana Kozachenko <kozachenkodiana03@mail.ru>
2022-11-24 19:28:27 +0700

    [DB] Testing cascad

--- price-map/apps/backend/src/app/modules/ws/ws.module.ts
+++ price-map/apps/backend/src/app/modules/ws/ws.module.ts
@@ -1,10 +1,10 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { Category3Level } from "@price-map/core/entities";
+import { Category3Level, Category2Level, Category1Level } from "@price-map/core/entities";
 import { WsGateway } from "./gateways/event.gateway";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category3Level], 'postgresConnect')],
+  imports: [TypeOrmModule.forFeature([Category3Level, Category2Level, Category1Level], 'postgresConnect')],
   providers: [WsGateway]
 })
 export class WsModule {}
