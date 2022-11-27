d0d32e5d508216e0d63057bf178310a8fa6f809f
Diana Kozachenko <kozachenkodiana03@mail.ru>
2022-11-27 16:55:15 +0700

    Parsing category filters in procces

--- price-map/apps/backend/src/app/modules/scraping/services/scraping.service.ts
+++ price-map/apps/backend/src/app/modules/scraping/services/scraping.service.ts
@@ -30,32 +30,6 @@ export class ScrapingService {
   }
 
   private async setCookies(): Promise<void> {
-    // await (this.driver.manage() as any).addCookie({name: "_yasc", value: "It7+VfsAEkQQ6+5y2bY/39GDw+x4bK4FrjdHSH6JvdMjCBSKxfB8kOXBcyhvAiVUYYQ=", domain: ".yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "_ym_d", value: "1669101421/yQUXLkON7IOuAkzlnLOutwD3Q", domain: ".yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "_ym_isad", value: "2", domain: ".yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "_ym_uid", value: "1664609197646862615", domain: ".yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "_ym_visorc", value: "b", domain: ".yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "bnpl_limit", value: "200000", domain: ".yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "cmp-merge", value: "true", domain: ".market.yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "currentRegionId", value: "65", domain: "market.yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "currentRegionName", value: "%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D0%B8%D0%B1%D0%B8%D1%80%D1%81%D0%BA", domain: "market.yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "fetch_loyalty_notifications_time_stamp", value: "2022-11-22T07:48:18.217Z", domain: ".yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "gdpr", value: "0", domain: ".yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "is_gdpr", value: "0", domain: ".yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "is_gdpr_b", value: "CMyzPRDqlgE=", domain: ".yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "js", value: "1", domain: "market.yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "mOC", value: "1", domain: "market.yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "nec", value: "0", domain: "market.yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "parent_reqid_seq", value: "1669101426989%2Fd6d0199893ed1daa275ba6f509ee0500%2C1669101443600%2Ff8c4170b15d711bb47d1a3f609ee0500", domain: ".market.yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "reviews-merge", value: "true", domain: ".market.yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "session_server_request_id_market:product/product--futbolka-suvenirshop-ghostbuster-gostbaster-maslennikov/1750369481", value: "1669101426989%2Fd6d0199893ed1daa275ba6f509ee0500", domain: "market.yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "spravka", value: "dD0xNjY5MTAzMjQyO2k9MTc4LjQ5LjI1My4yMjM7RD1GRDMzOENFRkM2RUJEQTlEMTE0OUM3MTQwNDhCOTQ1MDQyNUI0MDBFOERDNjJCODhDNEEwMUZERTZFMDQ5QTY4MTI3NkU3QzY7dT0xNjY5MTAzMjQyODk3NDY3NDIxO2g9NGUwM2I5OGI4M2FkMTRmMzNmMmU4Mzg4OTRjNjc5MTA=", domain: ".yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "skid", value: "3271347601669101427", domain: "market.yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "ugcp", value: "1", domain: "market.yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "visits", value: "1669103160-1669103160-1669103160", domain: ".market.yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "yandexuid", value: "96788151669101421", domain: ".yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "ymex", value: "1984461421.yrts.1669101421#1984461421.yrtsi.1669101421", domain: ".yandex.ru"});
-    // await (this.driver.manage() as any).addCookie({name: "yuidss", value: "96788151669101421", domain: ".yandex.ru"});
     await this.driver.manage().addCookie({name: "_yasc", value: "It7+VfsAEkQQ6+5y2bY/39GDw+x4bK4FrjdHSH6JvdMjCBSKxfB8kOXBcyhvAiVUYYQ=", domain: ".yandex.ru"});
     await this.driver.manage().addCookie({name: "_ym_d", value: "1669101421/yQUXLkON7IOuAkzlnLOutwD3Q", domain: ".yandex.ru"});
     await this.driver.manage().addCookie({name: "_ym_isad", value: "2", domain: ".yandex.ru"});
@@ -123,6 +97,7 @@ export class ScrapingService {
       
       for (const category2LevelDiv of category2LevelDivs) {
         const category2LevelDivHeading = await category2LevelDiv.findElement(By.css(this.category2LevelDivHeadingSelector));
+        //при переборе не получает элемент
         const category2LevelName = await category2LevelDivHeading.getText()
         const category2Level = {
           name: category2LevelName,
@@ -132,10 +107,124 @@ export class ScrapingService {
         const categories3LevelDivs = await category2LevelDiv.findElements(By.css(this.categories3LevelDivsSelector))
 
         if (categories3LevelDivs) {
-          for (const categories3LevelDiv of categories3LevelDivs) {
-            const cat3Name = await categories3LevelDiv.getText();
+          //for (const categories3LevelDiv of categories3LevelDivs) {
+            const actions = this.driver.actions({ async: true });
+            await actions.move({ origin: categories3LevelDivs[0] }).click().perform();
+
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            
+            const breadcrumb = await this.driver.findElements(By.css('ol[itemscope] li'));
+
+            const category1LevelNameTemp: string = await breadcrumb[0].getText();
+            const category2LevelNameTemp: string = await breadcrumb[1].getText();
+            const category3LevelNameTemp: string = await breadcrumb[2].getText();
+            console.log('path', category1LevelNameTemp, category2LevelNameTemp, category3LevelNameTemp)
+
+            const filters = [];
+
+            const filterDiv = await this.driver.findElement(By.css('div[data-grabber="SearchFilters"]'));
+            const filterDivsBoolean = await filterDiv.findElements(By.css('div[data-filter-type="boolean"]'));
+            const filterDivsEnum = await filterDiv.findElements(By.css('div[data-filter-type="enum"]'));
+            const filterDivsRange = await filterDiv.findElements(By.css('div[data-filter-type="range"]'));
+
+            //бульки
+            for (const filterDivBoolean of filterDivsBoolean) {
+              const filterBooleanName: string = await filterDivBoolean.getText();
+
+              filters.push({
+                name: filterBooleanName,
+                type: 'boolean'
+              })
+            // }
+
+            //рэнжи
+            for (const filterDivRange of filterDivsRange) {
+              const filterDivRangeLegend = await filterDivRange.findElement(By.css('fieldset span'));
+              const filterDivRangeName: string = await filterDivRangeLegend.getText();
+              const filterDivRangeMinLabel = await filterDivRange.findElement(By.css('span[data-auto="filter-range-min"] label:not([for])'));
+              const filterDivRangeMaxLabel = await filterDivRange.findElement(By.css('span[data-auto="filter-range-max"] label:not([for])'));
+
+              const filterDivRangeMinLabelText: string = await filterDivRangeMinLabel.getText();
+              const filterDivRangeMaxLabelText: string = await filterDivRangeMaxLabel.getText();
+
+              const filterDivRangeMinValueStr: string = filterDivRangeMinLabelText.split(' ')[1];
+              const filterDivRangeMaxValueStr: string = filterDivRangeMaxLabelText.split(' ')[1];
+
+              let filterRangeMinValue: number = 0;
+
+              if (filterDivRangeMinValueStr.includes(',')) {
+                filterRangeMinValue = Number.parseFloat(filterDivRangeMinValueStr.replace(',', '.'));
+              } else {
+                filterRangeMinValue = Number.parseInt(filterDivRangeMinValueStr);
+              }
+
+              let filterRangeMaxValue: number = 0;
+
+              if (filterDivRangeMaxValueStr.includes(',')) {
+                filterRangeMaxValue = Number.parseFloat(filterDivRangeMaxValueStr.replace(',', '.'));
+              } else {
+                filterRangeMaxValue = Number.parseInt(filterDivRangeMaxValueStr);
+              }
+
+              filters.push({
+                name: filterDivRangeName,
+                type: 'range',
+                value: [filterRangeMinValue, filterRangeMaxValue]
+              })
+            }
+
+            //енумки
+            for (const filterDivEnum of filterDivsEnum) {
+              const filterDivEnumLegend = await filterDivEnum.findElement(By.css('fieldset legend'));
+              const filterDivEnumName: string = await filterDivEnumLegend.getText();
+
+              await this.driver.manage().setTimeouts( { implicit: 3000 } );
+              const filterEnumFieldsetDivs = await filterDivEnum.findElements(By.css('fieldset > div > div'));
+
+              for (let fieldsetDiv of filterEnumFieldsetDivs) {
+                const moreSpans = await fieldsetDiv.findElements(By.css('span[tabindex="0"]'))
+
+                for (let moreSpan of moreSpans) {
+                  const actions = this.driver.actions({ async: true });
+                  await this.driver.actions().scroll(0, 0, 0, 0, moreSpan).perform()
+                  await actions.move({ origin: moreSpan }).click().perform();
+                  await this.driver.manage().setTimeouts( { implicit: 1000 } );
+                }                
+              }
+              
+              
+              
+
+              const filterEnumValueDivs = await filterDivEnum.findElements(By.css('fieldset div[data-baobab-name="FilterValue"]'));
+              console.log('filterEnumValueDivs', filterEnumValueDivs.length)
+              const filterValues: string[] = [];
+              let count = 0;
+              
+              for (const filterEnumValueDiv of filterEnumValueDivs) {
+                ++count;
+                const filterEnumValue: string = await filterEnumValueDiv.getText();
+                console.log('after', filterEnumValue)
+                if (count === 12) {
+                  await this.driver.actions().scroll(0, 0, 0, 200, filterEnumValueDiv).perform()
+                }
+                
+                console.log('after scroll', count)
+                filterValues.push(filterEnumValue);
+              }
+
+              filters.push({
+                name: filterDivEnumName,
+                type: 'enum',
+                value: filterValues
+              })
+            }
+
+            console.log('filters', filters)
+            await new Promise(resolve => setTimeout(resolve, 1330000));
+            const category3LevelName = await categories3LevelDivs[0].getText();
             category2Level.categories3Level.push({
-              name: cat3Name
+              name: category3LevelName,
+              filters
             })
           }
         }
@@ -158,6 +247,7 @@ export class ScrapingService {
       await this.driver.get('https://market.yandex.ru/');
       await this.setCookies();
 
+      //TODO: Есть вайбы, что все равно страница редиректит даже если нет капчи
       if (this.isShowedCaptcha()) {
         await this.driver.get('https://market.yandex.ru/');
       }
