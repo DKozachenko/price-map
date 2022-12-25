184da22dc96220b07d629d999655d23862e85ebf
Diana Kozachenko <kozachenkodiana03@mail.ru>
2022-12-25 20:16:44 +0700

    [filter-layout] Decomposed map on map service

--- price-map/apps/frontend/src/app/modules/map/map.module.ts
+++ price-map/apps/frontend/src/app/modules/map/map.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapComponent } from './components';
 import { MapRoutingModule } from './map-routing.module';
+import { MapService } from './services';
 
 @NgModule({
   declarations: [MapComponent],
   imports: [
     CommonModule,
     MapRoutingModule
-  ]
+  ],
+  providers: [MapService]
 })
 export class MapModule { }
