58656b4b1524f310eefe25d2c034a998d3a9baa9
Diana Kozachenko <kozachenkodiana03@mail.ru>
2023-02-11 19:50:15 +0700

    [PM-89] Added jquery for price control

--- price-map/apps/frontend/src/app/modules/map/components/price-control/price-control.component.ts
+++ price-map/apps/frontend/src/app/modules/map/components/price-control/price-control.component.ts
@@ -1,13 +1,45 @@
 import { FilterService } from './../../services';
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import 'round-slider';
 
 @Component({
   selector: 'map-price-control',
   templateUrl: './price-control.component.html',
   styleUrls: ['./price-control.component.scss']
 })
-export class PriceControlComponent {
+export class PriceControlComponent implements OnInit {
   public filterService: FilterService; 
 
-  
+  public ngOnInit(): void {
+    document.addEventListener('DOMContentLoaded', () => {
+      (<any>$('#slider')).roundSlider({
+        radius: 50,
+        circleShape: "pie",
+        sliderType: "min-range",
+        showTooltip: true,
+        editableTooltip: false,
+        handleSize: '+7',
+        startAngle: 315,
+        svgMode: true,
+        width: 10,
+        min: 50,
+        max: 156000,
+        borderWidth: 0,
+        pathColor: 'white',
+        rangeColor: '#a16eff',
+        tooltipColor: 'black',
+        change: "traceEvent",
+        drag: "traceEvent"
+      });
+
+      function traceEvent(e: any) {
+        console.log(e.type);
+      }
+    })
+  }
+
+  public traceEvent(e: any) {
+    console.log(e);
+  }
 }
