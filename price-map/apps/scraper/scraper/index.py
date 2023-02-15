8dade47d662cced656322afa84b97736048da386
Diana <kozachenkodiana03@mail.ru>
2023-02-15 17:46:01 +0700

    [PP] Added modules

--- price-map/apps/scraper/scraper/index.py
+++ price-map/apps/scraper/scraper/index.py
@@ -1,11 +1,2 @@
-# from './services/base' import BaseScrapingService
-# from base-scraping.service import BaseScrapingService
-# base = BaseScrapingService()
-# print(base)
-
-#works tvar
-# from test import testFunc
-# print(testFunc())
-
-from base_scraping_service import BaseScrapingService
+from services.base_scraping_service import BaseScrapingService
 print(4)
--- price-map/apps/scraper/scraper/test.py
+++
@@ -1,3 +0,0 @@
-def testFunc():
-  result = 5 + 6
-  return result
