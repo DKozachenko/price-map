7f85ee00cdf37cdc0113988c1f4bafb169c0db4a
Diana Kozachenko <kozachenkodiana03@mail.ru>
2023-02-18 21:21:03 +0700

    [PP] Changed version of nest microservices

--- price-map/apps/backend/src/main.ts
+++ price-map/apps/backend/src/main.ts
@@ -1,4 +1,4 @@
-import { INestApplication, Logger } from '@nestjs/common';
+import { INestApplication, INestMicroservice, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
@@ -7,22 +7,22 @@ async function bootstrap() {
   const app: INestApplication = await NestFactory.create(AppModule);
   const globalPrefix: string = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port: number | string = process.env.PORT || 3333;
 
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://quest:quest@localhost:5672'],
-      queue: 'test_exchange',
+      urls: ['amqp://guest:guest@localhost:5672'],
+      queue: 'test_queue',
       queueOptions: {
-        durable: true,
-      },
+        durable: true
+      }
     },
   });
 
-  await app.startAllMicroservices();
-  await app.listen(port);
   
+  app.startAllMicroservices();
+  const port: number | string = process.env.PORT || 3333;
+  await app.listen(port);
   Logger.log(
     `Server is running on: http://localhost:${port}/${globalPrefix}`,
     'bootstrap'
