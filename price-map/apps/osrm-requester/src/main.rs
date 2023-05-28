b375a563c52566126deaf61e0a402827413f1a25
Diana <kozachenkodiana03@mail.ru>
2023-05-28 22:26:39 +0700

    [PM-158] Logger service

--- price-map/apps/osrm-requester/src/main.rs
+++ price-map/apps/osrm-requester/src/main.rs
@@ -1,6 +1,7 @@
-use std::{borrow, fs, result::Result, error::Error};
-use amiquip::{Connection, ConsumerMessage, ConsumerOptions, Publish, QueueDeclareOptions, Exchange};
+use std::{process, fs, result::Result, error::Error, borrow};
+use amiquip::{Connection, ConsumerMessage, ConsumerOptions, Publish, QueueDeclareOptions, Exchange, Channel, Queue, Consumer};
 use geo_types::{LineString};
+use reqwest::blocking::Response;
 use serde::{Serialize, Deserialize};
 use polyline;
 use serde_yaml;
@@ -73,71 +74,106 @@ struct MessageData<'a> {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let logger: Logger = Logger::new();
-    logger.log("Hey", "hop");
-    logger.error("Hey 2", "hop 24");
+    logger.log("Application start", "main");
 
-    let config: Config = get_config()?;
-    println!("{:?}", config);
+    let config: Config = get_config().unwrap_or_else(|err| {
+        logger.error(format!("Error while getting config file: {}", err).as_str(), "main");
+        process::exit(-1);
+    });
 
-    // let mut connection = Connection::insecure_open("amqp://admin:admin_rabbit@localhost:5672").unwrap();
+    let mut connection: Connection = Connection::insecure_open("amqp://admin:admin_rabbit@localhost:5672").unwrap_or_else(|err| {
+        logger.error(format!("Error while setting connection: {}", err).as_str(), "main");
+        process::exit(-1);
+    });
 
-    // let channel = connection.open_channel(None).unwrap();
+    let channel: Channel = connection.open_channel(None).unwrap_or_else(|err| {
+        logger.error(format!("Error while openning channel: {}", err).as_str(), "main");
+        process::exit(-1);
+    });
 
-    // let queue = channel.queue_declare(config.request_queue, QueueDeclareOptions {
-    //   durable: true,
-    //   ..QueueDeclareOptions::default()
-    // }).unwrap();
+    let queue: Queue = channel.queue_declare(&config.request_queue, QueueDeclareOptions {
+      durable: true,
+      ..QueueDeclareOptions::default()
+    }).unwrap_or_else(|err| {
+        logger.error(format!("Error while getting queue: {}", err).as_str(), "main");
+        process::exit(-1);
+    });
 
 
-    // let consumer = queue.consume(ConsumerOptions::default()).unwrap();
-    // println!("Waiting for messages. Press Ctrl-C to exit.");
+    let consumer: Consumer = queue.consume(ConsumerOptions::default()).unwrap_or_else(|err| {
+        logger.error(format!("Error while closing channel: {}", err).as_str(), "main");
+        process::exit(-1);
+    });
 
-    // for (i, message) in consumer.receiver().iter().enumerate() {
-    //     match message {
-    //         ConsumerMessage::Delivery(delivery) => {
-    //             let body: borrow::Cow<str> = String::from_utf8_lossy(&delivery.body);
-    //             let deserialized: Message<Vec<Coordinates>> = serde_json::from_str(&body).unwrap();
-    //             println!("deserialized = {:?}", deserialized);
-    //             let coordinates_array: Vec<Coordinates> = deserialized.data;
-    //             println!("Coordinates string {:?}", coordinates_array);
-    //             consumer.ack(delivery).unwrap();
+    for (_i, message) in consumer.receiver().iter().enumerate() {
+        match message {
+            ConsumerMessage::Delivery(delivery) => {
+                logger.log(format!("Message from {}, content length: {}", &config.request_queue, delivery.body.len()).as_str(), "main");
+                let body: borrow::Cow<str> = String::from_utf8_lossy(&delivery.body);
+                let message: Message<Vec<Coordinates>> = serde_json::from_str(&body).unwrap_or_else(|err| {
+                    logger.error(format!("Error while deserializing message {}, error: {}", body, err).as_str(), "main");
+                    process::exit(-1);
+                });
+                consumer.ack(delivery).unwrap_or_else(|err| {
+                    logger.error(format!("Error while acking message: {}", err).as_str(), "main");
+                    process::exit(-1);
+                });
 
-    //             let coordinates_str: String = get_coordinates_str(coordinates_array);
+                let coordinates_str: String = get_coordinates_str(message.data);
 
-    //             let query: String = format!("http://router.project-osrm.org/route/v1/driving/{}?overview=full&steps=true", coordinates_str);
-    //             println!("{query}");
+                let url: String = format!("http://router.project-osrm.org/route/v1/driving/{}?overview=full&steps=true", coordinates_str);
 
-    //             let resp = reqwest::blocking::get(query).unwrap().json::<OsrmData>().unwrap();
-    //             // println!("{:#?}", resp);
+                logger.log(format!("Send http request to {}", url).as_str(), "main");
+                let response: Response = reqwest::blocking::get(&url).unwrap_or_else(|err| {
+                    logger.error(format!("Error while sending request: {}, error {}", url, err).as_str(), "main");
+                    process::exit(-1);
+                });
 
-    //             let mut decoded = polyline::decode_polyline(&resp.routes[0].geometry, 5).unwrap();
+                let osrm_data: OsrmData = response.json::<OsrmData>().unwrap_or_else(|err| {
+                    logger.error(format!("Error while deserializing data from response: {}", err).as_str(), "main");
+                    process::exit(-1);
+                });
 
-    //             let coordinates = map_line_string(decoded);
+                let decoded_line_string: LineString = polyline::decode_polyline(&osrm_data.routes[0].geometry, 5).unwrap_or_else(|err| {
+                    logger.error(format!("Error while decoding polyline: {}, error {}", &osrm_data.routes[0].geometry, err).as_str(), "main");
+                    process::exit(-1);
+                });
 
-    //             let message = Message {
-    //                 data: MessageData {
-    //                     coordinates,
-    //                     legs: &resp.routes[0].legs
-    //                 },
-    //                 description: String::from("Успешное построение маршрута"),
-    //                 send_time: Local::now().to_string()
-    //             };
+                let coordinates: Vec<Vec<f64>> = get_coordinates(decoded_line_string);
 
-    //             let serialized = serde_json::to_string(&message).unwrap();
-    //             // println!("serialized = {}", serialized);
+                let message: Message<MessageData> = Message {
+                    data: MessageData {
+                        coordinates,
+                        legs: &osrm_data.routes[0].legs
+                    },
+                    description: String::from("Успешное построение маршрута"),
+                    send_time: Local::now().to_string()
+                };
 
-    //             let exchange = Exchange::direct(&channel);
-    //             exchange.publish(Publish::new(serialized.as_bytes(), &config.response_queue)).unwrap();
-    //             println!("message {}", config.response_queue);
-    //         }
-    //         other => {
-    //             println!("Consumer ended: {:?}", other);
-    //             break;
-    //         }
-    //     }
-    // }
+                let message_str: String = serde_json::to_string(&message).unwrap_or_else(|err| {
+                    logger.error(format!("Error while serializing message: {:?}, error {}", message, err).as_str(), "main");
+                    process::exit(-1);
+                });
 
-    // connection.close().unwrap();
+                let exchange: Exchange = Exchange::direct(&channel);
+                let body: &[u8] = message_str.as_bytes();
+                exchange.publish(Publish::new(body, &config.response_queue)).unwrap_or_else(|err| {
+                    logger.error(format!("Error while publishing message: {}, error {}", message_str, err).as_str(), "main");
+                    process::exit(-1);
+                });
+                logger.log(format!("Send message to {}, content length: {}", &config.response_queue, body.len()).as_str(), "main");
+            }
+            other => {
+                logger.log(format!("Consumer ended: {:?}", other).as_str(), "main");
+                break;
+            }
+        }
+    }
+
+    connection.close().unwrap_or_else(|err| {
+        logger.error(format!("Error while closing connection: {}", err).as_str(), "main");
+        process::exit(-1);
+    });
 
     Ok(())
 }
@@ -163,15 +199,15 @@ fn get_coordinates_str(coordinates_array: Vec<Coordinates>) -> String {
   result
 }
 
-fn map_line_string(line_string: LineString) -> Vec<Vec<f64>> {
-    let mut result = Vec::new();
+fn get_coordinates(line_string: LineString) -> Vec<Vec<f64>> {
+    let mut result: Vec<Vec<f64>> = Vec::new();
 
-    for coord in line_string {
-        let mut coord_vec = Vec::new();
-        coord_vec.push(coord.x);
-        coord_vec.push(coord.y);
+    for coordinate in line_string {
+        let mut coordinate_vector: Vec<f64> = Vec::new();
+        coordinate_vector.push(coordinate.x);
+        coordinate_vector.push(coordinate.y);
 
-        result.push(coord_vec);
+        result.push(coordinate_vector);
     }
 
     result
